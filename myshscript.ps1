c7a64b6a47e413fbcc07f042b3306d202b8701d9
Rafael Fernando <rafafernando793@gmail.com>
2021-02-24 22:21:31 -0300

    Update myshscript.ps1

--- myshscript.ps1
+++ myshscript.ps1
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
-
 cd C:\
-mkdir C:\feitopeloscript
+mkdir C:\feitopeloscript2
 
 cd C:\Windows\System32\Sysprep
 sysprep /oobe /generalize /shutdown /unattend:C:\unattend.xml
 Restart-computer
 
+Add-WindowsFeature Web-Server
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) 22!"
