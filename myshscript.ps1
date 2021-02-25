405773fcc263d2c10d2927e97af8e12093f84aff
Rafael Fernando <rafafernando793@gmail.com>
2021-02-24 22:16:24 -0300

    Update myshscript.ps1

--- myshscript.ps1
+++ myshscript.ps1
@@ -5,3 +5,8 @@ Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host
 
 cd C:\
 mkdir C:\feitopeloscript
+
+cd C:\Windows\System32\Sysprep
+sysprep /oobe /generalize /shutdown /unattend:C:\unattend.xml
+Restart-computer
+
